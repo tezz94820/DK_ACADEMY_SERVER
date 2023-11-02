960f3033fea0834cefe1843e84ce21741706cd5a
Tejas Itankar <tejasitankar94820@gmail.com>
2023-11-02 18:55:08 +0530

    changed the github actions

--- pm2.config.js
+++ pm2.config.js
@@ -3,9 +3,6 @@ module.exports = {
       {
         script: 'src/app.ts',
         interpreter: '/home/ubuntu/.nvm/versions/node/v20.9.0/lib/node_modules/ts-node/dist/bin.js',
-        watch: true,
-        exec_mode: 'fork',
-        instances: 1,
         name: 'dk_academy_server',
       }
     ]
