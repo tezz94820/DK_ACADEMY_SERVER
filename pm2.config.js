07d6aed9809b042c8c93e9bd9fd912ad780443d6
Tejas Itankar <tejasitankar94820@gmail.com>
2023-11-02 10:47:57 +0530

    changed from cluster to fork in pm2

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
     script: 'src/app.ts',
     interpreter: '/home/ubuntu/.nvm/versions/node/v20.9.0/lib/node_modules/ts-node/dist/bin.js',
     watch: true,
-    exec_mode: 'cluster',
+    exec_mode: 'fork',
     instances: 1,
     name: 'dk_academy_server', // Replace with your desired app name
 };
