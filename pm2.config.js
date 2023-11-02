e161220d1f9de8109bded2eb139ded0f076dcdb6
Tejas Itankar <tejasitankar94820@gmail.com>
2023-11-02 13:09:00 +0530

    cahnged the approach for github actions

--- pm2.config.js
+++ pm2.config.js
@@ -5,5 +5,16 @@ module.exports = {
     exec_mode: 'fork',
     instances: 1,
     name: 'dk_academy_server', // Replace with your desired app name
+    env: {
+        PORT: process.env.PORT,
+        NODE_ENV: process.env.NODE_ENV,
+        MONGO_URI: process.env.MONGO_URI,
+        JWT_SECRET: process.env.JWT_SECRET,
+        JWT_LIFETIME: process.env.JWT_LIFETIME,
+        FAST2SMS_API_KEY: process.env.FAST2SMS_API_KEY,
+        CRYPT_PASSWORD: process.env.CRYPT_PASSWORD,
+        CRYPT_IV: process.env.CRYPT_IV
+    },
+    args: ['--update-env']
 };
   
