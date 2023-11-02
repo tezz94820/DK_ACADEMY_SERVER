6676182708793db2efce2932136357677eff2067
Tejas Itankar <tejasitankar94820@gmail.com>
2023-11-02 18:23:42 +0530

    used create-envfile to create .env file

---
+++ pm2.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        script: 'src/app.ts',
+        interpreter: '/home/ubuntu/.nvm/versions/node/v20.9.0/lib/node_modules/ts-node/dist/bin.js',
+        watch: true,
+        exec_mode: 'fork',
+        instances: 1,
+        name: 'dk_academy_server',
+      }
+    ]
+  };
+  
