f129c6c02f0363b1c7894d9557938d8f46643f76
Tejas Itankar <tejasitankar94820@gmail.com>
2023-11-02 10:16:49 +0530

    checking the CI of github actions

--- pm2.config.js
+++ pm2.config.js
@@ -3,6 +3,6 @@ module.exports = {
     name: 'dk_academy_server',
     exec_mode: 'cluster', // Optional: Use 'cluster' mode for better performance
     instances: 1, // Optional: Set the number of instances based on your needs
-    // other PM2 configuration options as needed
+    // other PM2 configuration options as needed pm2
 };
   
