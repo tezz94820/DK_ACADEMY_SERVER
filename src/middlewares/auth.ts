8b4debcd2651cbf72152a2db127589c6e0bf63a2
Tejas Itankar <tejasitankar94820@gmail.com>
2023-11-20 13:19:16 +0530

    added try catch in auth protect middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -9,26 +9,28 @@ export interface AuthenticatedRequest extends Request {
   }
 
 const Protect =  catchAsync( async (req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> => {
-    // Getting token and check of it's there
-    let token:string;
-    if ( req.headers.authorization && req.headers.authorization.startsWith('Bearer') ) 
-        token = req.headers.authorization.split(' ')[1];
-    
-    if (!token) 
-        return sendError(res, 401, 'You are not logged in! Please log in to get access.', {});
-    
+    try {
+        // Getting token and check of it's there
+        let token:string;
+        if ( req.headers.authorization && req.headers.authorization.startsWith('Bearer') ) 
+            token = req.headers.authorization.split(' ')[1];
+            
+        if (!token) 
+            return sendError(res, 401, 'You are not logged in! Please log in to get access.', {});
 
-    // Verifiying token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
-    if(!decoded)
-        return sendError(res,401,'user cannot be found. please relogin',401);
-    const user = await Student.findById(decoded.userId);
-    if(!user)
-        return sendError(res,401,'user cannot be found. please relogin',401);
+        // Verifiying token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
+        if(!decoded)
+            return sendError(res,401,'user cannot be found. please relogin',401);
+        const user = await Student.findById(decoded.userId);
+        if(!user)
+            return sendError(res,401,'user cannot be found. please relogin',401);
 
-    req.user = user;
-    
-    next();
+        req.user = user;
+        next();
+    } catch (error) {
+        sendError(res, 401, 'Access Token not valid', {});
+    }
 });
 
 export { Protect };
