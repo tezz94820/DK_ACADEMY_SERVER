ac023f0d3e13932b5abdd71c7750268c6884507e
Tejas Itankar <tejasitankar94820@gmail.com>
2023-12-04 03:57:43 +0530

    changed the cors policy of server

--- src/utils/corsOptions.ts
+++ src/utils/corsOptions.ts
@@ -1,5 +1,9 @@
 const corsOptions = {
-    origin:'*', 
+    origin: [
+        'https://dkacademy.co.in',
+        'https://www.dkacademy.co.in',
+        'http://localhost:3000'
+    ],
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
 }
