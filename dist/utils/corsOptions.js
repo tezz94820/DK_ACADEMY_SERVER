243c355eb73b4d852302b6335b62de920657ce13
Tejas Itankar <tejasitankar94820@gmail.com>
2023-12-04 10:49:57 +0530

    deleting dist folder

--- dist/utils/corsOptions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200,
-};
-exports.default = corsOptions;
-//# sourceMappingURL=corsOptions.js.map
